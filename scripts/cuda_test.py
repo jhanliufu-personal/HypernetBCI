9f12f2134e98608c4743a8bb939a730bb35406fe
jhan <mliufu@uchicago.edu>
2024-04-21 15:37:11 -0500

    data download problem resolved; was bc of mne version difference. test cuda on bbq

---
+++ scripts/cuda_test.py
@@ -0,0 +1,12 @@
+import torch
+
+cuda = torch.cuda.is_available()  # check if GPU is available, if True chooses to use it
+device = 'cuda' if cuda else 'cpu'
+if cuda:
+    torch.backends.cudnn.benchmark = True
+# Set random seed to be able to roughly reproduce results
+# Note that with cudnn benchmark set to True, GPU indeterminism
+# may still make results substantially different between runs.
+# To obtain more consistent results at the cost of increased computation time,
+# you can set `cudnn_benchmark=False` in `set_random_seeds`
+# or remove `torch.backends.cudnn.benchmark = True`
--- scripts/dataset_download.py
+++ scripts/dataset_download.py
@@ -1,31 +1,4 @@
-import matplotlib.pyplot as plt
 from braindecode.datasets import MOABBDataset
-from numpy import multiply
-from braindecode.preprocessing import (Preprocessor,
-                                       exponential_moving_standardize,
-                                       preprocess)
-from braindecode.preprocessing import create_windows_from_events
-import torch
-from braindecode.models import ShallowFBCSPNet
-from braindecode.util import set_random_seeds
-from skorch.callbacks import LRScheduler
-from skorch.helper import predefined_split
-from braindecode import EEGClassifier
-# from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-import pandas as pd
-from scipy import stats
-import os
-import pickle
-from matplotlib.lines import Line2D
-# from braindecode.visualization import plot_confusion_matrix
-
-from braindecode.datasets import BaseConcatDataset
-from braindecode.datasets.base import EEGWindowsDataset
-from braindecode.preprocessing.windowers import _create_windows_from_events
-import numpy as np
-import mne
-import random
 
 # This will download data to default path, ~mne_data.
 # Need to figure out how to specify download path
