5b4beb4904889cbd21e5012b5a34fc6849a5bf93
jhan <mliufu@uchicago.edu>
2024-04-21 15:39:54 -0500

    added some print statements to cuda_test script

--- scripts/cuda_test.py
+++ scripts/cuda_test.py
@@ -3,7 +3,9 @@ import torch
 cuda = torch.cuda.is_available()  # check if GPU is available, if True chooses to use it
 device = 'cuda' if cuda else 'cpu'
 if cuda:
+    print("Nvidia CUDA is available")
     torch.backends.cudnn.benchmark = True
+print("Only CPU available")
 # Set random seed to be able to roughly reproduce results
 # Note that with cudnn benchmark set to True, GPU indeterminism
 # may still make results substantially different between runs.
