9e00c02ef8ae49162ceff4b3d98fe5e1d26a38ef
jhan <mliufu@uchicago.edu>
2024-04-21 15:49:52 -0500

    try download Schirr* dataset again

--- scripts/dataset_download.py
+++ scripts/dataset_download.py
@@ -3,6 +3,6 @@ from braindecode.datasets import MOABBDataset
 # This will download data to default path, ~mne_data.
 # Need to figure out how to specify download path
 # BNCI2014_001 has 9 subjects
-BNCI2014_001_dataset = MOABBDataset(dataset_name="BNCI2014_001", subject_ids=list(range(1, 10)))
+# BNCI2014_001_dataset = MOABBDataset(dataset_name="BNCI2014_001", subject_ids=list(range(1, 10)))
 # Schirrmeister2017 has 14 subjects
 Schirrmeister2017_dataset = MOABBDataset(dataset_name="Schirrmeister2017", subject_ids=list(range(1, 15)))
