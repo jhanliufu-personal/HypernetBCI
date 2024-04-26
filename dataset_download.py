4b840fbc3527f0a3f3a2f4e1cab25d8741fae0cd
jhan <mliufu@uchicago.edu>
2024-04-26 01:15:16 -0500

    download SleepPhysioNet to /data/projects

--- dataset_download.py
+++ dataset_download.py
@@ -1,8 +1,10 @@
-from braindecode.datasets import MOABBDataset
+from braindecode.datasets import MOABBDataset, SleepPhysionet
 
-# This will download data to default path, ~mne_data.
-# Need to figure out how to specify download path
 # BNCI2014_001 has 9 subjects
-BNCI2014_001_dataset = MOABBDataset(dataset_name="BNCI2014_001", subject_ids=list(range(1, 10)))
+# BNCI2014_001_dataset = MOABBDataset(dataset_name="BNCI2014_001", subject_ids=list(range(1, 10)))
+
 # Schirrmeister2017 has 14 subjects
-Schirrmeister2017_dataset = MOABBDataset(dataset_name="Schirrmeister2017", subject_ids=list(range(1, 15)))
+# Schirrmeister2017_dataset = MOABBDataset(dataset_name="Schirrmeister2017", subject_ids=list(range(1, 15)))
+
+# SleepPhysionet has 78 subjects and 2 recordings per subject
+sleepphysionet_dataset = SleepPhysionet(subject_ids=range(79), recording_ids=[1, 2,], crop_wake_mins=30)
