1ffb597b5ddd0cd3a34c36aea9670c3f5c82e0ec
jhan <mliufu@uchicago.edu>
2024-07-08 22:42:42 -0500

    repeat baseline_2_6, save classifier weights after fine tuning

--- MI_baseline_2_torch.py
+++ MI_baseline_2_torch.py
@@ -18,9 +18,6 @@ from braindecode.preprocessing import (Preprocessor,
 from braindecode.preprocessing import create_windows_from_events
 import torch
 from braindecode.util import set_random_seeds
-from skorch.callbacks import LRScheduler
-from skorch.helper import predefined_split
-from braindecode import EEGClassifier
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import stats
@@ -31,7 +28,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 
 from utils import (
-    get_subset, import_model, clf_predict_on_set, parse_training_config, 
+    get_subset, import_model, parse_training_config, 
     freeze_param, train_one_epoch, test_model
 )
 
@@ -47,12 +44,32 @@ dataset = MOABBDataset(dataset_name=args.dataset_name, subject_ids=subject_ids_l
 
 print('Data loaded')
 
-results_file_name = f'{args.model_name}_{args.dataset_name}_finetune_{args.experiment_version}'
 dir_results = 'results/'
+experiment_folder_name = f'{args.model_name}_{args.dataset_name}_finetune_{args.experiment_version}'
+results_file_name = f'{experiment_folder_name}_results'
+intermediate_outputs_file_name = f'{experiment_folder_name}_intermediate_outputs'
+accuracy_figure_file_name = f'{experiment_folder_name}_accuracy'
+results_file_path = os.path.join(
+    dir_results, 
+    f'{experiment_folder_name}/', 
+    f'{results_file_name}.pkl'
+)
+intermediate_outputs_file_path = os.path.join(
+    dir_results, 
+    f'{experiment_folder_name}/', 
+    f'{intermediate_outputs_file_name}.pkl'
+)
+accuracy_figure_file_path = os.path.join(
+    dir_results, 
+    f'{experiment_folder_name}/', 
+    f'{accuracy_figure_file_name}.png'
+)
+print(f'Saving results at {results_file_path}')
+print(f'Saving intermediate outputs at {intermediate_outputs_file_path}')
+print(f'Saving accuracy figure at {accuracy_figure_file_path}')
+
 # used to store pre-trained model parameters
 temp_exp_name = f'baseline_2_{args.experiment_version}_pretrain'
-file_path = os.path.join(dir_results, f'{results_file_name}.pkl')
-print(f'Saving results at {file_path}')
 
 ### ----------------------------- Plotting parameters -----------------------------
 if args.data_amount_unit == 'trial':
@@ -130,6 +147,8 @@ splitted_by_subj = windows_dataset.split('subject')
 dict_results = {}
 results_columns = ['valid_accuracy',]
 
+dict_intermediate_outputs = {}
+
 for holdout_subj_id in subject_ids_lst:
     
     print(f'Hold out data from subject {holdout_subj_id}')
@@ -144,15 +163,27 @@ for holdout_subj_id in subject_ids_lst:
 
     # Check if a pre-trained model exists
     # shouldn't have har coded it. Need to think of a better way to use pretrained models from other experiment
-    temp_exp_name = 'baseline_2_6_pretrain'
+    # temp_exp_name = 'baseline_2_6_pretrain'
     # check if a pretrained model exists
-    model_param_path = os.path.join(dir_results, f'{temp_exp_name}_without_subj_{holdout_subj_id}_model_params.pth')
+    model_param_path = os.path.join(
+        dir_results, 
+        f'{experiment_folder_name}/',
+        f'{temp_exp_name}_without_subj_{holdout_subj_id}_model_params.pth'
+    )
+    pretrain_curve_path = os.path.join(
+        dir_results, 
+        f'{experiment_folder_name}/',
+        f'{temp_exp_name}_without_subj_{holdout_subj_id}_pretrain_curve.png'
+    )
     model_exist = os.path.exists(model_param_path) and os.path.getsize(model_param_path) > 0
 
     if model_exist:
         if args.only_pretrain:
             continue
     else:
+        print(f'Pretraining model for subject {holdout_subj_id}')
+        print(f'Hold out data from subject {holdout_subj_id}')
+
         ### ---------- Split pre-train set into pre-train-train set and pre-train-test set ----------
         ### THIS PART IS FOR BCNI2014001
         if args.dataset_name == 'BCNI2014001':
@@ -208,20 +239,50 @@ for holdout_subj_id in subject_ids_lst:
         pre_train_train_loader = DataLoader(pre_train_train_set, batch_size=args.batch_size, shuffle=True)
         pre_train_test_loader = DataLoader(pre_train_test_set, batch_size=args.batch_size)
 
+        pretrain_train_acc_lst = []
+        pretrain_test_acc_lst = []
         for epoch in range(1, args.n_epochs + 1):
             print(f"Epoch {epoch}/{args.n_epochs}: ", end="")
+
             train_loss, train_accuracy = train_one_epoch(
-                pre_train_train_loader, cur_model, loss_fn, optimizer, scheduler, epoch, device,
+                pre_train_train_loader, 
+                cur_model, 
+                loss_fn, 
+                optimizer, 
+                scheduler, 
+                epoch, 
+                device,
+                print_batch_stats=False
             )
-            test_loss, test_accuracy = test_model(pre_train_test_loader, cur_model, loss_fn)
+
+            test_loss, test_accuracy = test_model(
+                pre_train_test_loader, 
+                cur_model, 
+                loss_fn,
+                print_batch_stats=False
+            )
             print(
                 f"Train Accuracy: {100 * train_accuracy:.2f}%, "
                 f"Average Train Loss: {train_loss:.6f}, "
                 f"Test Accuracy: {100 * test_accuracy:.1f}%, "
                 f"Average Test Loss: {test_loss:.6f}\n"
             )
+
+        # Plot and save the pretraining curve
+        plt.figure()
+        plt.plot(pretrain_train_acc_lst, label='Training accuracy')
+        plt.plot(pretrain_test_acc_lst, label='Test accuracy')
+        plt.legend()
+        plt.xlabel('Training epochs')
+        plt.ylabel('Accuracy')
+        plt.title(f'{temp_exp_name}_without_subj_{holdout_subj_id}_pretrain_curve')
+        plt.savefig(pretrain_curve_path)
+
         # Save the model weights to a file
-        torch.save(cur_model.state_dict(), model_param_path)
+        torch.save(
+            cur_model.state_dict(), 
+            model_param_path
+        )
 
     ### -----------------------------------------------------------------------------------------
     ### ---------------------------------------- FINE TUNING ------------------------------------
@@ -255,11 +316,13 @@ for holdout_subj_id in subject_ids_lst:
 
     ### Finetune with different amount of new data
     dict_subj_results = {0: [finetune_baseline_acc,]}
+    dict_subj_intermediate_outputs = {}
     finetune_trials_num = len(finetune_subj_train_set.get_metadata())
     for finetune_training_data_amount in np.arange(1, (finetune_trials_num // args.data_amount_step) + 1) * args.data_amount_step:
 
-        final_accuracy = []
-        
+        final_accuracy_lst = []
+        final_tensor_lst = []
+
         ### Since we're sampling randomly, repeat for 'repetition' times
         for i in range(args.repetition):
 
@@ -289,9 +352,10 @@ for holdout_subj_id in subject_ids_lst:
                     freeze_param(finetune_model, param_name)
 
             # Continue training / fine tuning
-            print(f'Fine tuning model for subject {holdout_subj_id}' +
-                  f'with {len(cur_finetune_subj_train_subset)} trials (repetition {i})' +
-                  f'with lr = {args.fine_tune_lr:.5f}'
+            print(
+                f'Fine tuning model for subject {holdout_subj_id}' +
+                f'with {len(cur_finetune_subj_train_subset)} trials (repetition {i})' +
+                f'with lr = {args.fine_tune_lr:.5f}'
             )
 
             finetune_optimizer = torch.optim.AdamW(
@@ -326,25 +390,66 @@ for holdout_subj_id in subject_ids_lst:
                     f"Average Test Loss: {test_loss:.6f}\n"
                 )
         
-        dict_subj_results.update({finetune_training_data_amount: np.mean(test_accuracy_lst[-5:])})
+            final_accuracy_lst.append(np.mean(test_accuracy_lst[-5:]))
+            # Save weights of the classifier after fine tuning
+            final_tensor_lst.append(finetune_model.final_layer.conv_classifier.weight)
 
-    dict_results.update({holdout_subj_id: dict_subj_results})
+        dict_subj_results.update(
+            {
+                finetune_training_data_amount: final_accuracy_lst
+            }
+        )
+
+        dict_subj_intermediate_outputs.update(
+            {
+                finetune_training_data_amount: {
+                    'final_tensor': final_tensor_lst
+                }
+            }
+        )
+
+    dict_results.update(
+        {
+            holdout_subj_id: dict_subj_results
+        }
+    )
+
+    dict_intermediate_outputs.update(
+        {
+            holdout_subj_id: dict_subj_intermediate_outputs
+        }
+    )
+
     ### ----------------------------- Save results -----------------------------
     # Save results after done with a subject, in case server crashes
-    # remove existing results file if one exists
-    if os.path.exists(file_path):
-        os.remove(file_path)
+    # remove existing pickle file if one exists
+    if os.path.exists(results_file_path):
+        os.remove(results_file_path)
     # save the updated one
-    with open(file_path, 'wb') as f:
+    with open(results_file_path, 'wb') as f:
         pickle.dump(dict_results, f)
 
+    if os.path.exists(intermediate_outputs_file_path):
+        os.remove(intermediate_outputs_file_path)
+    # save the updated one
+    with open(intermediate_outputs_file_path, 'wb') as f:
+        pickle.dump(dict_intermediate_outputs, f)
+
 # check if results are saved correctly
-if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
-    with open(file_path, 'rb') as f:
+if os.path.exists(results_file_path) and os.path.getsize(results_file_path) > 0:
+    with open(results_file_path, 'rb') as f:
         dummy = pickle.load(f)
-    print("Data was saved successfully.")
+    print("Results were saved successfully.")
 else:
-    print(f"Error: File '{file_path}' does not exist or is empty. The save was insuccesful")
+    print(f"Error: File '{results_file_path}' does not exist or is empty. The save was insuccesful")
+
+# check if intermediate outputs are saved correctly
+if os.path.exists(intermediate_outputs_file_path) and os.path.getsize(intermediate_outputs_file_path) > 0:
+    with open(intermediate_outputs_file_path, 'rb') as f:
+        dummy = pickle.load(f)
+    print("Intermediate outputs were saved successfully.")
+else:
+    print(f"Error: File '{intermediate_outputs_file_path}' does not exist or is empty. The save was insuccesful")
 
 ### -----------------------------------------------------------------------------------------
 ### ---------------------------------------- PLOTTING ---------------------------------------
@@ -396,5 +501,8 @@ for ax in [ax1, ax2, ax3]:
     ax.set_xlabel(f'Fine tune data amount ({args.data_amount_unit})')
     ax.set_ylabel('Accuracy')
 
-plt.suptitle(f'{args.model_name} on {args.dataset_name} Dataset \n fine tune model for each subject, {args.repetition} reps each point')
-plt.savefig(os.path.join(dir_results, f'{results_file_name}.png'))
+plt.suptitle(
+    f'{args.model_name} on {args.dataset_name} Dataset \n, ' 
+    f'fine tune model for each subject, {args.repetition} reps each point'
+)
+plt.savefig(accuracy_figure_file_path)
