44b00dd8bc54cd7da552375d6c043711112364bc
jhan <mliufu@uchicago.edu>
2024-09-25 08:05:03 -0500

    debug MI_ensemble_baseline

--- MI_ensemble_baseline.py
+++ MI_ensemble_baseline.py
@@ -26,7 +26,7 @@ warnings.filterwarnings('ignore')
 args = parse_training_config()
 # model_object = import_model(args.model_name)
 # subject_ids_lst = list(range(1, 14))
-subject_ids_lst = [1, 2]
+subject_ids_lst = [1,]
 
 preprocessed_dir = 'data/Schirrmeister2017_preprocessed'
 if os.path.exists(preprocessed_dir) and os.listdir(preprocessed_dir):
